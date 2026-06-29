8caa773844d1cf45d8f3b661a9eadf6c8dff600b
ne0nur <ne0nur@users.noreply.github.com>
2026-06-29 17:21:18 +0200

    feat: replace world cup country flags with livescore crests

--- src/lib/teamLogos.ts
+++ src/lib/teamLogos.ts
@@ -111,29 +111,78 @@ export function getTeamLogo(teamName: string): string {
     clean = 'borussiadortmund'
   }
 
-  // Country Flags Mapping (ISO 3166-1 alpha-2 for flagcdn)
+  // World Cup 2026 Team Crests (Livescore + Fallbacks)
   const COUNTRY_FLAGS: Record<string, string> = {
-    'mexico': 'mx', 'southafrica': 'za', 'southkorea': 'kr', 'czechia': 'cz',
-    'canada': 'ca', 'bosniaherzegovina': 'ba', 'unitedstates': 'us', 'paraguay': 'py',
-    'qatar': 'qa', 'switzerland': 'ch', 'brazil': 'br', 'morocco': 'ma', 'haiti': 'ht',
-    'scotland': 'gb-sct', 'australia': 'au', 'turkiye': 'tr', 'turkey': 'tr',
-    'germany': 'de', 'curacao': 'cw', 'netherlands': 'nl', 'japan': 'jp',
-    'ivorycoast': 'ci', 'ecuador': 'ec', 'sweden': 'se', 'tunisia': 'tn',
-    'spain': 'es', 'capeverde': 'cv', 'belgium': 'be', 'egypt': 'eg',
-    'saudiarabia': 'sa', 'uruguay': 'uy', 'iran': 'ir', 'newzealand': 'nz',
-    'france': 'fr', 'senegal': 'sn', 'iraq': 'iq', 'norway': 'no',
-    'argentina': 'ar', 'algeria': 'dz', 'austria': 'at', 'jordan': 'jo',
-    'portugal': 'pt', 'congodr': 'cd', 'england': 'gb-eng', 'croatia': 'hr',
-    'ghana': 'gh', 'panama': 'pa', 'uzbekistan': 'uz', 'colombia': 'co',
+    'mexico': 'https://lsm-static-prod.livescore.com/medium/enet/6710.png',
+    'southafrica': 'https://flagcdn.com/za.svg',
+    'southkorea': 'https://lsm-static-prod.livescore.com/medium/enet/7804.png',
+    'czechia': 'https://lsm-static-prod.livescore.com/medium/enet/8496.png',
+    'canada': 'https://lsm-static-prod.livescore.com/medium/enet/5810.png',
+    'bosniaherzegovina': 'https://lsm-static-prod.livescore.com/medium/enet/10106.png',
+    'unitedstates': 'https://lsm-static-prod.livescore.com/medium/enet/6713.png',
+    'paraguay': 'https://lsm-static-prod.livescore.com/medium/enet/6724.png',
+    'qatar': 'https://lsm-static-prod.livescore.com/medium/enet/5902.png',
+    'switzerland': 'https://lsm-static-prod.livescore.com/medium/enet/6717.png',
+    'brazil': 'https://lsm-static-prod.livescore.com/medium/enet/8256.png',
+    'morocco': 'https://lsm-static-prod.livescore.com/medium/enet/6262.png',
+    'haiti': 'https://lsm-static-prod.livescore.com/medium/enet/5934.png',
+    'scotland': 'https://lsm-static-prod.livescore.com/medium/enet/8498.png',
+    'australia': 'https://lsm-static-prod.livescore.com/medium/enet/6716.png',
+    'turkiye': 'https://lsm-static-prod.livescore.com/medium/enet/8276.png', // manual fix (Turkey)
+    'turkey': 'https://lsm-static-prod.livescore.com/medium/enet/8276.png',
+    'germany': 'https://lsm-static-prod.livescore.com/medium/enet/8570.png',
+    'curacao': 'https://lsm-static-prod.livescore.com/medium/enet/287981.png',
+    'netherlands': 'https://lsm-static-prod.livescore.com/medium/enet/6708.png',
+    'japan': 'https://lsm-static-prod.livescore.com/medium/enet/6715.png',
+    'ivorycoast': 'https://lsm-static-prod.livescore.com/medium/enet/6709.png',
+    'ecuador': 'https://lsm-static-prod.livescore.com/medium/enet/6707.png',
+    'sweden': 'https://lsm-static-prod.livescore.com/medium/enet/8520.png',
+    'tunisia': 'https://lsm-static-prod.livescore.com/medium/enet/6719.png',
+    'spain': 'https://lsm-static-prod.livescore.com/medium/enet/6720.png',
+    'capeverde': 'https://lsm-static-prod.livescore.com/medium/enet/5888.png',
+    'belgium': 'https://lsm-static-prod.livescore.com/medium/enet/8263.png',
+    'egypt': 'https://lsm-static-prod.livescore.com/medium/enet/10255.png',
+    'saudiarabia': 'https://lsm-static-prod.livescore.com/medium/enet/7795.png',
+    'uruguay': 'https://lsm-static-prod.livescore.com/medium/enet/5796.png',
+    'iran': 'https://lsm-static-prod.livescore.com/medium/enet/6711.png',
+    'newzealand': 'https://flagcdn.com/nz.svg',
+    'france': 'https://lsm-static-prod.livescore.com/medium/enet/6723.png',
+    'senegal': 'https://lsm-static-prod.livescore.com/medium/enet/6395.png',
+    'iraq': 'https://lsm-static-prod.livescore.com/medium/enet/5819.png',
+    'norway': 'https://lsm-static-prod.livescore.com/medium/enet/8492.png',
+    'argentina': 'https://lsm-static-prod.livescore.com/medium/enet/6706.png',
+    'algeria': 'https://lsm-static-prod.livescore.com/medium/enet/6265.png', // manual fix
+    'austria': 'https://lsm-static-prod.livescore.com/medium/enet/8255.png',
+    'jordan': 'https://a.espncdn.com/i/teamlogos/countries/500/jor.png',
+    'portugal': 'https://lsm-static-prod.livescore.com/medium/enet/8361.png',
+    'congodr': 'https://lsm-static-prod.livescore.com/medium/enet/6321.png',
+    'england': 'https://lsm-static-prod.livescore.com/medium/enet/8491.png',
+    'croatia': 'https://lsm-static-prod.livescore.com/medium/enet/10155.png',
+    'ghana': 'https://lsm-static-prod.livescore.com/medium/enet/6714.png',
+    'panama': 'https://a.espncdn.com/i/teamlogos/countries/500/pan.png',
+    'uzbekistan': 'https://lsm-static-prod.livescore.com/medium/enet/8700.png',
+    'colombia': 'https://lsm-static-prod.livescore.com/medium/enet/8258.png',
+
     // deutsche namen fallback
-    'deutschland': 'de', 'spanien': 'es', 'frankreich': 'fr', 'italien': 'it',
-    'niederlande': 'nl', 'belgien': 'be', 'osterreich': 'at', 'schweiz': 'ch',
-    'sudafrika': 'za', 'argentinien': 'ar', 'brasilien': 'br', 'algerien': 'dz',
-    'jordanien': 'jo', 'kanada': 'ca', 'turkei': 'tr'
+    'deutschland': 'https://lsm-static-prod.livescore.com/medium/enet/8570.png',
+    'spanien': 'https://lsm-static-prod.livescore.com/medium/enet/6720.png',
+    'frankreich': 'https://lsm-static-prod.livescore.com/medium/enet/6723.png',
+    'italien': 'https://lsm-static-prod.livescore.com/medium/enet/8261.png',
+    'niederlande': 'https://lsm-static-prod.livescore.com/medium/enet/6708.png',
+    'belgien': 'https://lsm-static-prod.livescore.com/medium/enet/8263.png',
+    'osterreich': 'https://lsm-static-prod.livescore.com/medium/enet/8255.png',
+    'schweiz': 'https://lsm-static-prod.livescore.com/medium/enet/6717.png',
+    'sudafrika': 'https://flagcdn.com/za.svg',
+    'argentinien': 'https://lsm-static-prod.livescore.com/medium/enet/6706.png',
+    'brasilien': 'https://lsm-static-prod.livescore.com/medium/enet/8256.png',
+    'algerien': 'https://lsm-static-prod.livescore.com/medium/enet/6265.png',
+    'jordanien': 'https://a.espncdn.com/i/teamlogos/countries/500/jor.png',
+    'kanada': 'https://lsm-static-prod.livescore.com/medium/enet/5810.png',
+    'turkei': 'https://lsm-static-prod.livescore.com/medium/enet/8276.png'
   }
 
   if (COUNTRY_FLAGS[clean]) {
-    return `https://flagcdn.com/${COUNTRY_FLAGS[clean]}.svg`
+    return COUNTRY_FLAGS[clean]
   }
 
   if (KNOWN_LOGOS.has(clean)) {
