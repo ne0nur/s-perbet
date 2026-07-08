4a8321d8b64eb2e9bdb3df780c6933ca59539385
ne0nur <ne0nur@users.noreply.github.com>
2026-07-08 16:58:55 +0200

    Updates

---
+++ assets/RivalAnalysisPage-Cv54rf02.js
@@ -0,0 +1,1 @@
+import{ot as e,vt as t,yt as n}from"./vendor-core-CPcSQnn5.js";import{t as r}from"./RivalInspector-CKB1rSFg.js";var i=e();function a(){let{userId:e}=n(),a=t();return e?(0,i.jsx)(`div`,{className:`min-h-screen bg-background px-3 pt-4 pb-24 max-w-xl mx-auto w-full animate-page-enter`,children:(0,i.jsx)(r,{userId:e,onClose:()=>a(-1)})}):null}export{a as RivalAnalysisPage};
